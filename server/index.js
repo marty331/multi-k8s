8ad95bf83ee7e56df1e79d98fa299f68f194f2c2
Marty Ballard <marty331@gmail.com>
2021-02-11 22:23:22 -0600

    remove bodyparser

--- server/index.js
+++ server/index.js
@@ -2,12 +2,11 @@ const keys = require('./keys');
 
 // Express App Setup
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Postgres Client Setup
 const { Pool } = require('pg');
